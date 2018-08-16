cf9d8a6719ec0812f269cac9176f2d176f9031d5
Marcos Escudero <marcosescudero@hotmail.com>
2018-08-16 01:15:32 -0300

    Backend & Api

---
+++ Sales.Common/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Sales.Common.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public decimal Price { get; set; } // Siempre conbiene utilizar "decimal" para manejar valores monetarios en C#
+        public bool IsAvailable { get; set; }
+        public DateTime PublishOn { get; set; }
+
+    }
+}
